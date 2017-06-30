2ea70132b8507e9a4d4f8887e904567d7c6f640d
Victor Ferraz <victorfsf.dev@gmail.com>
2017-06-30 16:13:33 -0300

    [master] Adding stemming to the IntentClassifier
    + Updating iob.model

--- eva/intents/train.py
+++ eva/intents/train.py
@@ -1,8 +1,10 @@
 from eva.utils import IOBReader
 from functools import partialmethod
+from nltk.stem import SnowballStemmer
 from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
+from nltk.corpus import stopwords
 
 
 class IntentClassifier(LinearSVC):
@@ -13,10 +15,17 @@ class IntentClassifier(LinearSVC):
             test_size=kwargs.pop('test_size', 0.2),
             random_state=kwargs.pop('random_state', 42)
         )
-        self.tfidf = TfidfVectorizer()
         self.x_features, self.x_labels = zip(*reader.train_set)
         self.y_features, self.y_labels = zip(*reader.test_set)
-        x_tfidf = self.tfidf.fit_transform(self.x_features)
+        # Stemming
+        self.stemmer = SnowballStemmer(language='portuguese')
+        self.stemmer.stopwords = set(stopwords.words('portuguese'))
+        # TF-IDF
+        self.tfidf = TfidfVectorizer()
+        x_tfidf = self.tfidf.fit_transform([
+            self.stemmer.stem(x) for x in self.x_features
+            if x not in self.stemmer.stopwords
+        ])
         return super().fit(x_tfidf, self.x_labels)
 
     def predict(self, features):
@@ -24,7 +33,11 @@ class IntentClassifier(LinearSVC):
             raise AttributeError(
                 'The model must be trained with fit() first.'
             )
-        return super().predict(self.tfidf.transform(features))
+        features = self.tfidf.transform([
+            self.stemmer.stem(x) for x in features
+            if x not in self.stemmer.stopwords
+        ])
+        return super().predict(features)
 
     def _get_evaluations(self, fn, feature_set):
         features, labels = zip(*feature_set)
@@ -44,6 +57,8 @@ class IntentClassifier(LinearSVC):
     )
 
     def __repr__(self):
+        if not hasattr(self, 'tfidf'):
+            return super().__repr__()
         return '%s(accuracy=%s, features=%s, labels=%s)' % (
             self.__class__.__name__,
             self.accuracy(zip(self.y_features, self.y_labels)),
