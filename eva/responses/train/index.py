7d5c08b64d17640bf1a5c58b37c00b7e35049d86
Victor Ferraz <victorfsf.dev@gmail.com>
2018-01-09 10:55:33 -0300

    Update index.py

--- eva/responses/train/index.py
+++ eva/responses/train/index.py
@@ -1,25 +1,78 @@
 from collections import defaultdict
-from eva.utils.mixins import SerializeMixin
-from eva.utils import regex_tokenize
+from difflib import SequenceMatcher
 from gensim import corpora
 from gensim import models
 from gensim import similarities
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-from sklearn.datasets.base import Bunch
-from toolz import frequencies
-import regex as re
-
-__all__ = [
-    'LSIndexer',
-]
+from unicodedata import normalize
+import pickle
+import re
 
 
-class LSIndexer(SerializeMixin):
+def normalize_ascii(value):
+    try:
+        return normalize('NFKD', str(value)) \
+            .encode('ascii', 'ignore').decode('ascii')
+    except Exception:
+        return value
+
+
+def regex_tokenize(sentence):
+    return list(filter(None, map(
+        normalize_ascii,
+        re.split(
+            r'\s|\t|\n|\r|\?|\-|\_|\.|\,|\!|\(|\)',
+            sentence
+        )
+    )))
+
+
+def ratio(a, b):
+    return SequenceMatcher(a=a.upper(), b=b.upper()).ratio()
+
+
+def remove_uf(doc):
+    return ' '.join(doc.split()[1:])
+
+
+class Bunch(dict):
+
+    def __init__(self, **kwargs):
+        super(Bunch, self).__init__(kwargs)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __dir__(self):
+        return self.keys()
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setstate__(self, state):
+        pass
+
+
+class LSIndexer:
 
     def __init__(self, *args, **kwargs):
         self.sections = defaultdict(Bunch)
         self.spellers = LSSpeller()
+
+    def save(self, path):
+        with open(path, 'wb') as f:
+            pickle.dump(self, f)
+        return self
+
+    def load(self, path):
+        with open(path, 'rb') as f:
+            instance = pickle.load(f)
+            self.__dict__ = instance.__dict__
+        return self
 
     def fit(self, section, documents, **kwargs):
         sec = self.sections[section]
@@ -35,24 +88,17 @@ class LSIndexer(SerializeMixin):
 
     def build(self, section, texts, **kwargs):
         sec = self.sections[section]
-        frequency = kwargs.pop('frequency', 0)
-        if frequency > 0:
-            freq_dict = frequencies([y for x in texts for y in x])
-            texts = [
-                [y for y in x if freq_dict[y] > frequency]
-                for x in texts
-            ]
         sec.dictionary = corpora.Dictionary(texts)
         sec.corpus = [
             sec.dictionary.doc2bow(text)
             for text in texts
         ]
-
         sec.tfidf = models.TfidfModel(sec.corpus)
         sec.lsi = models.LsiModel(
             sec.tfidf[sec.corpus],
             id2word=sec.dictionary,
-            num_topics=kwargs.pop('num_topics', 250)
+            num_topics=kwargs.pop('num_topics', 200),
+            power_iters=kwargs.pop('power_iters', 2)
         )
         sec.index = similarities.MatrixSimilarity(
             sec.lsi[sec.corpus]
@@ -70,17 +116,22 @@ class LSIndexer(SerializeMixin):
             if word not in self.stemmer.stopwords
         ]
 
-    def similarities(self, section, document):
+    def similarities(self, section, document,
+                     weight=0.5, limit=100, min_score=0.1):
         stem = self.transform(section, document)
         sec = self.sections[section]
         lsi = sec.lsi[sec.dictionary.doc2bow(stem)]
-        return [(sec.documents[x], y) for x, y in sorted(
-            enumerate(sec.index[lsi]),
-            key=lambda item: -item[1]
-        )]
+        highest_score = sorted((
+            (sec.documents[_id], score)
+            for _id, score in enumerate(sec.index[lsi])
+        ), key=lambda item: -item[1])[:limit]
+        return sorted(((
+            doc, score, (score + ratio(doc, document) * weight)
+        ) for doc, score in highest_score if score >= min_score
+        ), key=lambda item: -item[2])
 
-    def search(self, section, document):
-        similarities = self.similarities(section, document)
+    def search(self, section, document, **kwargs):
+        similarities = self.similarities(section, document.strip(), **kwargs)
         if similarities:
             return similarities[0][0]
         return None
